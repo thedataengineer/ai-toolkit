26ca81d9df394f460e07734018acbc8b1a77f2aa
Aswani Karteek Yadavilli <21700785+thedataengineer@users.noreply.github.com>
2025-02-17 23:07:13 -0500

    Update server.js log message to remove localhost reference for production

--- backend/server.js
+++ backend/server.js
@@ -14,4 +14,4 @@ app.get("/", (req, res) => {
     res.send("Server is running!");
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
